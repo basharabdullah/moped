e4f5b1c24281ab702c3750258567450701762114
Bashar Abdullah <bashar.abdullah@gmail.com>
2012-08-18 07:27:42 +0300

    add debug messages

--- lib/moped/node.rb
+++ lib/moped/node.rb
@@ -209,8 +209,9 @@ module Moped
     # @since 1.0.0
     def initialize(address)
       @address = address
-
+      puts "Address: #{address}"
       host, port = address.split(":")
+      puts "Host: #{host}\t Port: #{port}"
       @ip_address = ::Socket.getaddrinfo(host, nil, ::Socket::AF_INET, ::Socket::SOCK_STREAM).first[3]
       @port = port.to_i
       @resolved_address = "#{@ip_address}:#{@port}"
